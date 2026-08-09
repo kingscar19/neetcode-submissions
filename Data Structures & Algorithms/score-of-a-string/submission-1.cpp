fc71722a3059a389ea0c897c11e395a06d2578ad
Sai Ingale <inglesai813@gmail.com>
2026-08-09 12:25:31 +0530

    Add: score-of-a-string - submission-1

---
+++ Data Structures & Algorithms/score-of-a-string/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int n = s.length();
+        int ans = 0;
+
+        for(int i=0; i<n-1; i++) {
+            char c1 = s[i];
+            char c2 = s[i+1];
+            // int asciiValue1 = static_cast<int>(c1);  c++ a
+            // int asciiValue2 = static_cast<int>(c2);
+            ans = ans + abs(c2 - c1);
+        }
+        return ans;
+    }
+};
