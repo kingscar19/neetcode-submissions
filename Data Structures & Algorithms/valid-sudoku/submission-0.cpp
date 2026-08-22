faaf83fa6a89f986b44a25f41909f0b106933a6d
Sai Ingale <inglesai813@gmail.com>
2026-08-22 10:35:32 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<int> cols[9];
+        unordered_set<int> rows[9];
+        unordered_set<int> squares[9];
+
+        for(int r = 0; r < 9; r++) {
+            for(int c = 0; c < 9; c++) {
+                if(board[r][c] == '.') {
+                    continue;
+                }
+
+                int idx = (r / 3) * 3 + (c / 3);
+
+                if(rows[r].find(board[r][c]) != rows[r].end() ||
+                   cols[c].find(board[r][c]) != cols[c].end() ||
+                   squares[idx].find(board[r][c]) != squares[idx].end()) {
+                    return false;
+                }
+                cols[c].insert(board[r][c]);
+                rows[r].insert(board[r][c]);
+                squares[idx].insert(board[r][c]);
+            }
+        }
+        return true;
+    }
+};
